95e00102f3dcbd6e886e8b0411d92f6ac673b4c3
PCraftDev <pcraft7265@gmail.com>
2024-10-12 19:49:09 +0700

    Update sketch_oct06b.ino

--- esp32/sketch_oct06b.ino
+++ esp32/sketch_oct06b.ino
@@ -6,137 +6,208 @@
 #include <SPI.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <WebServer.h>
+#include <EEPROM.h>
 
 // Koneksi Wi-Fi
-const char* ssid = "P";
-const char* password = "hxlnc@91";
-
-// Informasi endpoint API Flask
-const char* serverName = "http://10.163.129.118:5000/data"; // Ganti IP dengan IP server Flask
+char ssid[32] = "P";
+char password[32] = "hxlnc@91";
+char api_server[128] = "http://10.163.129.118:5000/data";
 
 // Pin untuk DHT dan Relay
-#define DHTPIN 15      // Pin untuk DHT11
-#define DHTTYPE DHT11  // Tipe sensor DHT
-#define RELAY_PIN 5    // Pin untuk relay pompa
-#define LIGHT_CONTROL_PIN 16 // Pin untuk mengontrol lampu
+#define DHTPIN 15
+#define DHTTYPE DHT11
+#define RELAY_PIN 5
+#define LIGHT_CONTROL_PIN 16
 
 // Pin configuration for TFT
-#define TFT_CS     4
-#define TFT_RST    26   // Or set to -1 and connect to ESP32 RESET pin
-#define TFT_DC     27
+#define TFT_CS 4
+#define TFT_RST 26
+#define TFT_DC 27
 
 // Color definitions
-#define BLACK   ST77XX_BLACK
-#define RED     ST77XX_RED
-#define GREEN   ST77XX_GREEN
-#define BLUE    ST77XX_BLUE
-#define WHITE   ST77XX_WHITE
-#define YELLOW  ST77XX_YELLOW
+#define BLACK ST77XX_BLACK
+#define RED ST77XX_RED
+#define GREEN ST77XX_GREEN
+#define BLUE ST77XX_BLUE
+#define WHITE ST77XX_WHITE
+#define YELLOW ST77XX_YELLOW
 
-// Initialize the TFT display
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
-// Inisialisasi NTPClient
+// Inisialisasi NTPClient dan TFT
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200, 60000); // 25200 adalah offset waktu untuk GMT+7 (WIB), refresh setiap 60 detik
-
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200, 60000);
 DHT dht(DHTPIN, DHTTYPE);
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+WebServer server(80);
+
+// Fungsi untuk menyimpan konfigurasi
+void saveConfig(const char* newSSID, const char* newPassword, const char* newAPIServer) {
+  EEPROM.writeString(0, newSSID);
+  EEPROM.writeString(32, newPassword);
+  EEPROM.writeString(64, newAPIServer);
+  EEPROM.commit();
+  Serial.println("Konfigurasi Wi-Fi dan API disimpan ke EEPROM.");
+}
+
+// Fungsi untuk memuat konfigurasi
+void loadConfig() {
+  String savedSSID = EEPROM.readString(0);
+  String savedPassword = EEPROM.readString(32);
+  String savedAPIServer = EEPROM.readString(64);
+
+  if (savedSSID.length() > 0 && savedPassword.length() > 0) {
+    savedSSID.toCharArray(ssid, 32);
+    savedPassword.toCharArray(password, 32);
+    Serial.println("Konfigurasi Wi-Fi dimuat.");
+  }
+
+  if (savedAPIServer.length() > 0) {
+    savedAPIServer.toCharArray(api_server, 128);
+    Serial.println("Konfigurasi API dimuat.");
+  }
+}
+
+// Fungsi untuk memulai Access Point
+void startAccessPoint() {
+  WiFi.softAP("ESP32_AP", "123456789");
+  Serial.println("Access Point aktif. IP: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+// Fungsi untuk memulai Web Server
+void startWebServer() {
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html", R"rawliteral(
+      <html><body>
+      <h1>Konfigurasi Wi-Fi dan API</h1>
+      <form action="/save" method="POST">
+        SSID:<br><input type="text" name="ssid" value=")rawliteral" + String(ssid) + R"rawliteral("><br>
+        Password:<br><input type="text" name="password" value=")rawliteral" + String(password) + R"rawliteral("><br>
+        API Server:<br><input type="text" name="api_server" value=")rawliteral" + String(api_server) + R"rawliteral("><br>
+        <input type="submit" value="Simpan">
+      </form></body></html>)rawliteral");
+  });
+
+  server.on("/save", HTTP_POST, []() {
+    String newSSID = server.arg("ssid");
+    String newPassword = server.arg("password");
+    String newAPIServer = server.arg("api_server");
+
+    if (newSSID.length() > 0 && newPassword.length() > 0 && newAPIServer.length() > 0) {
+      newSSID.toCharArray(ssid, 32);
+      newPassword.toCharArray(password, 32);
+      newAPIServer.toCharArray(api_server, 128);
+
+      saveConfig(ssid, password, api_server);
+      server.send(200, "text/html", "Konfigurasi tersimpan! Restart...");
+      delay(2000);
+      ESP.restart();
+    } else {
+      server.send(200, "text/html", "Input tidak valid.");
+    }
+  });
+
+  server.begin();
+  Serial.println("Web server aktif.");
+}
+
+// Fungsi untuk koneksi ke Wi-Fi
+void connectToWiFi() {
+  Serial.println("Menghubungkan ke Wi-Fi...");
+  WiFi.begin(ssid, password);
+
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nTerhubung ke Wi-Fi. IP:");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nGagal menghubungkan ke Wi-Fi.");
+    startAccessPoint();
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
 
-  // Set relay sebagai output
+  // Set relay dan kontrol lampu sebagai output
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(LIGHT_CONTROL_PIN, OUTPUT); // Set pin untuk lampu
+  pinMode(LIGHT_CONTROL_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH); // Pompa off (HIGH = OFF)
   digitalWrite(LIGHT_CONTROL_PIN, LOW); // Lampu off (LOW = OFF)
 
   // Inisialisasi TFT
-  tft.initR(INITR_BLACKTAB); // Initialize ST7735R chip
-  tft.fillScreen(BLACK); // Fill screen with black
-
-  // Set text size and color for TFT
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(BLACK);
   tft.setTextSize(1);
   tft.setTextColor(WHITE);
-
-  // Display static title on TFT
   tft.setCursor(10, 10);
   tft.print("Monitoring Suhu & Kelembapan");
 
-  // Koneksi Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+  // Inisialisasi EEPROM
+  EEPROM.begin(192);
+  loadConfig();
 
-  // Inisialisasi NTPClient
+  // Koneksi ke Wi-Fi
+  connectToWiFi();
+
+  // Inisialisasi NTP
   timeClient.begin();
+
+  // Memulai web server
+  startWebServer();
 }
 
 void loop() {
   // Perbarui waktu dari NTP
   timeClient.update();
+  server.handleClient(); // Handle HTTP request
 
-  if(WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED) {
     // Membaca suhu dan kelembapan
     float temp = dht.readTemperature();
     float hum = dht.readHumidity();
 
-    if (isnan(temp) || isnan(hum)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
+    if (!isnan(temp) && !isnan(hum)) {
+      displayData(temp, hum);
+
+      // Buat JSON untuk dikirim ke API
+      String jsonData = "{\"temperature\": " + String(temp) + ", \"humidity\": " + String(hum) + "}";
+      Serial.println("Mengirim data: " + jsonData);
+
+      // Kirim data ke server API
+      HTTPClient http;
+      http.begin(api_server);
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST(jsonData);
+
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("Respon dari server: " + response);
+      } else {
+        Serial.println("Gagal mengirim data: " + String(httpResponseCode));
+      }
+      http.end();
+    } else {
+      Serial.println("Gagal membaca dari sensor DHT.");
     }
-
-    // Menampilkan data ke layar TFT
-    displayData(temp, hum);
-
-    // Membuat payload JSON
-    String jsonData = "{\"temperature\": " + String(temp) + ", \"humidity\": " + String(hum) + "}";
-    Serial.println("Sending data: " + jsonData);
-
-    // Inisialisasi HTTPClient
-    HTTPClient http;
-    http.begin(serverName); // Endpoint API Flask
-    http.addHeader("Content-Type", "application/json"); // Header untuk JSON
-
-    // Kirim data dengan POST request
-    int httpResponseCode = http.POST(jsonData);
-
-    // Cek hasil response dari server
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("HTTP Response code: " + String(httpResponseCode));
-      Serial.println("Response from server: " + response);
-    }
-    else {
-      Serial.println("Error sending POST: " + String(httpResponseCode));
-    }
-
-    // End HTTP connection
-    http.end();
+  } else {
+    Serial.println("WiFi tidak terhubung.");
   }
-  else {
-    Serial.println("WiFi not connected");
-  }
-
-  // Cek apakah ada perintah dari Arduino melalui serial
-    // Cek apakah ada perintah dari Arduino melalui serial
 
   delay(1000); // Kirim data setiap 1 detik
 }
 
-// Function to display data on TFT screen
+// Fungsi untuk menampilkan data di layar TFT
 void displayData(float temperature, float humidity) {
-  // Clear previous readings
   tft.fillRect(0, 30, 128, 128, BLACK);
-
-  // Draw a separator line
   tft.drawLine(0, 30, 128, 30, WHITE);
 
-  // Display temperature
   tft.setCursor(25, 40);
   tft.setTextColor(YELLOW);
   tft.setTextSize(1.5);
@@ -144,7 +215,6 @@ void displayData(float temperature, float humidity) {
   tft.print(temperature);
   tft.print("C");
 
-  // Display humidity
   tft.setCursor(5, 55);
   tft.setTextColor(GREEN);
   tft.setTextSize(1.5);
@@ -152,34 +222,27 @@ void displayData(float temperature, float humidity) {
   tft.print(humidity);
   tft.print(" %");
 
-  // Get the current time and date from NTPClient
-  unsigned long epochTime = timeClient.getEpochTime();
-  String timeString = timeClient.getFormattedTime();
-  String dateString = getFormattedDate(epochTime);
-
-  // Display time
+  // Tampilkan waktu dari NTP
   tft.setCursor(20, 70);
   tft.setTextColor(WHITE);
   tft.setTextSize(1);
   tft.print("Waktu: ");
-  tft.print(timeString);
+  tft.print(timeClient.getFormattedTime());
 
-  // Display date
+  // Tampilkan tanggal
   tft.setCursor(10, 80);
   tft.print("Tanggal: ");
-  tft.print(dateString);
+  tft.print(getFormattedDate(timeClient.getEpochTime()));
 }
 
-// Function to format the date from epoch time
+// Fungsi untuk format tanggal
 String getFormattedDate(unsigned long epochTime) {
-  // Konversi epoch time ke tanggal
   time_t rawTime = epochTime;
-  struct tm *timeinfo;
-  timeinfo = localtime(&rawTime);
+  struct tm *timeinfo = localtime(&rawTime);
 
   String dayStr = String(timeinfo->tm_mday);
-  String monthStr = String(timeinfo->tm_mon + 1); // tm_mon is 0-11
-  String yearStr = String(timeinfo->tm_year + 1900); // tm_year is years since 1900
+  String monthStr = String(timeinfo->tm_mon + 1);
+  String yearStr = String(timeinfo->tm_year + 1900);
 
   // Tambahkan leading zero untuk tanggal dan bulan
   if (timeinfo->tm_mday < 10) dayStr = "0" + dayStr;
